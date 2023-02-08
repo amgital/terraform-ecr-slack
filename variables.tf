10553f553f083fa9f0279c407b83e9d5a08014a5
Andrei Merfu <andrei.merfu@sparktech.ro>
2023-02-08 09:38:35 +0200

    Fix data type

--- variables.tf
+++ variables.tf
@@ -10,9 +10,7 @@ variable "aws_region" {
   type = string
 }
 
-data "aws_caller_identity" "current" {
-  type = string
-}
+data "aws_caller_identity" "current" {}
 
 variable "tags" {
   type = map(string)
